8fba96fc94f41e31e631ba58f48358893f0a45cf
david baik <kinggo96@gmail.com>
2017-04-04 15:37:23 +0800

    simple_linkedlist.c
    Signed-off-by: david baik <kinggo96@gmail.com>

---
+++ simple_linkedlist.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct list
+{
+	int num;
+	struct list *next; //자기 참조 구조체
+};
+
+void main()
+{
+	struct list a = { 10 }, b{ 20 }, c{ 30 };
+
+#if 0
+	struct list *tp = &a;
+	struct list *head = &a;
+	tp->next = &b;
+	tp->next->next = &c;
+
+	printf("\na:%d", tp->num);
+	printf("\na:%d", tp->next->num);
+	printf("\na:%d", tp->next->next->num);
+
+	while (tp != NULL)
+	{
+		printf("\na:%d", tp->num);
+		tp = tp->next;
+	}
+	
+	tp = head;
+	printf("\na:%d", tp->num);
+#else
+	a.next = &b;
+	b.next = &c;
+	printf("\na:%d", a.num);
+	printf("\na:%d", a.next->num);
+	printf("\na:%d", a.next->next->num);
+
+#endif
+
+}
